bc78d0decf1d308b3b7770c2dbab446481dfa8b5
marekreid <marekreid@gmail.com>
2021-10-05 08:46:23 +0200

    update

--- src/components/landing/heroImage.js
+++ src/components/landing/heroImage.js
@@ -1,30 +1,14 @@
 import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { StaticImage } from "gatsby-plugin-image"
+
 const HeroImage = () => {
 
-      
 
-const data = useStaticQuery(graphql`
-query MyQuery {
-  allFile(filter: {name: {in: "hero"}}) {
-    edges {
-      node {
-        id
-        childImageSharp {
-          gatsbyImageData
-        }
-      }
-    }
-  }
-}
 
-`)
-
-const image = getImage(data.allFile.edges[0].node.childImageSharp)
     return (
-   
-       <GatsbyImage image={image} alt="bleh" style={{width: `100%`,  maxHeight: `100%`}}/>
+      <StaticImage src="../../images/hero.png" alt="A kitten" style={{width: `100%`,  maxHeight: `100%`}} />
+       
      
     )
 }
