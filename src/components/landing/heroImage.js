cea40b5141e0a982ad623bfd7d8f68a326c438b8
marekreid <marekreid@gmail.com>
2021-10-05 08:48:45 +0200

    update

--- src/components/landing/heroImage.js
+++ src/components/landing/heroImage.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-const HeroImage = () => {
+const HeroImage = ({device}) => {
 
 
 
     return (
-      <StaticImage src="../../images/hero.png" alt="A kitten" style={{width: `100%`,  maxHeight: `100%`}} />
+      <StaticImage src="../../images/hero.png" alt="A kitten" style={{width: `100%`,  maxHeight: `100%`, aspectRatio: device === "mob" ? `1` : ``}} />
        
      
     )
