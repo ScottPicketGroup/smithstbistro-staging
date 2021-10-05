2b16bb4d589c8fcc5adcbb193bd7f34ba16cea8b
marekreid <marekreid@gmail.com>
2021-10-05 08:31:27 +0200

    update

--- src/components/landing/heroImage.js
+++ src/components/landing/heroImage.js
@@ -1,35 +1,52 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-
+import { StaticQuery, graphql } from "gatsby"
 const HeroImage = ({  device }) => {
-  const data = useStaticQuery(graphql`
-  query MyQuery {
-    allFile(filter: { name: { in: "hero" } }) {
-      edges {
-        node {
-          id
-          childImageSharp {
-            gatsbyImageData
+//   const data = useStaticQuery(graphql`
+//   query MyQuery {
+//     allFile(filter: { name: { in: "hero" } }) {
+//       edges {
+//         node {
+//           id
+//           childImageSharp {
+//             gatsbyImageData
+//           }
+//         }
+//       }
+//     }
+//   }
+// `);
+  // const image = getImage(data.allFile.edges[0].node);
+  
+
+    return (
+      <StaticQuery
+      query={graphql`
+      query MyQuery {
+        allFile(filter: { name: { in: "hero" } }) {
+          edges {
+            node {
+              id
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
           }
         }
       }
-    }
-  }
-`);
-  const image = getImage(data.allFile.edges[0].node);
-  
-  if (data) {
-    return (
-    
-      <GatsbyImage
-        image={image}
+      `}
+      render={data => (
+        <GatsbyImage
+        image={getImage(data.allFile.edges[0].node)}
         alt="bleh"
         style={{ width: `100%`, maxHeight: `100%`, aspectRatio: device === 'mob' ? `1/1` : ``}}
         
       />
+      )}
+    />
+     
     );
-  }
+  
 };
 
 export default HeroImage;
